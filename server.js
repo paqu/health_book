b64d43b519fca1e01098569ee0386529c82e21a5
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2016-01-05 00:56:58 +0100

    Remove unnecessary things from scaffoldg

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var colors = require('colors');
-
-http.createServer(function (req,res) {
-	res.writeHead(200, {'Content-Type':'text/plain'});
-	res.end('Hello World \n');
-}).listen(1234,'127.0.0.1');
-console.log('Server running at http://127.0.0.1.:1234/'.green);
