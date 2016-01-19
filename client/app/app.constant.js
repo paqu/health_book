bda09daba3980f268ce672f4c3eb8eb51a4153f6
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2016-01-19 22:58:58 +0100

    Remove unnecessary things in scaffolding

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('ksiazeczkaZdrowiaApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
