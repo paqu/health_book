a37d6f02e793efe044880d56a041f3bb3a68d4ad
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2016-01-22 14:33:45 +0100

    Create basic schema for patient

--- server/api/patient/patient.model.js
+++ server/api/patient/patient.model.js
@@ -4,9 +4,18 @@ var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 var Schema = mongoose.Schema;
 
 var PatientSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+    childInfo:{
+      surname: String,
+      firstname:String,
+      dateOfBirth: {
+          day:Number,
+          mounth:Number,
+          year:Number,
+      },
+      placeOfBirth:String,
+      address:String,
+      pesel:Number,
+    }
 });
 
 module.exports = mongoose.model('Patient', PatientSchema);
