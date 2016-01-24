62f17d7f8e5a2b3a940c23aed270a943fba38f0a
Maciej Sobkowski <maciejjo@maciejjo.pl>
2016-01-24 02:01:52 +0100

    Add new controllers suporting edit, remove and create new patient

--- client/app/patients/patients.controller.js
+++ client/app/patients/patients.controller.js
@@ -3,4 +3,32 @@
 angular.module('ksiazeczkaZdrowiaApp')
   .controller('PatientsCtrl', function ($scope,Patient) {
     $scope.patients = Patient.query();
-  });
+  })
+  .controller('PatientViewCtrl',function ($scope, $state, $stateParams, Patient) {
+      console.log("XXXXXXXXXXXXXXXXXXXX");
+      console.log($stateParams.id);
+      console.log("XXXXXXXXXXXXXXXXXXXX");
+    $scope.patient = Patient.get({id: $stateParams.id});
+
+    $scope.deletePatient = function (){
+        Patient.remove({id: $stateParams.id});
+        $state.go('patients');
+    }
+  })
+  .controller('PatientNewCtrl',function ($scope, $state, Patient) {
+    $scope.patient = {};
+
+    $scope.addPatient = function (){
+        Patient.create($scope.patient);
+        $state.go('patients');
+    }
+  })
+  .controller('PatientEditCtrl',function ($scope, $state,$stateParams, Patient) {
+    $scope.patient = Patient.get({id: $stateParams.id});
+
+    $scope.editPatient = function (){
+        Patient.update($scope.patient);
+        $state.go('patients');
+    }
+  })
+;
