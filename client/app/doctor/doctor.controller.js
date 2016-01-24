4d9c7a4c0d053a5ba8b6b4dedee8a54e7fe99caf
Maciej Sobkowski <maciejjo@maciejjo.pl>
2016-01-24 03:17:45 +0100

    Add sample schema for doctor controller and route state

--- client/app/doctor/doctor.controller.js
+++ client/app/doctor/doctor.controller.js
@@ -3,4 +3,30 @@
 angular.module('ksiazeczkaZdrowiaApp')
   .controller('DoctorCtrl', function ($scope) {
     $scope.message = 'Hello';
-  });
+  })
+  .controller('DoctorViewCtrl',function ($scope, $state, $stateParams, Doctor) {
+      console.log($stateParams.id);
+    $scope.patient = Doctor.get({id: $stateParams.id});
+
+    $scope.deleteDoctor = function (){
+        Doctor.remove({id: $stateParams.id});
+        $state.go('doctors');
+    }
+  })
+  .controller('DoctorNewCtrl',function ($scope, $state, Doctor) {
+    $scope.patient = {};
+
+    $scope.addDoctor = function (){
+        Doctor.create($scope.patient);
+        $state.go('doctors');
+    }
+  })
+  .controller('DoctorEditCtrl',function ($scope, $state,$stateParams, Doctor) {
+    $scope.patient = Doctor.get({id: $stateParams.id});
+
+    $scope.editDoctor = function (){
+        Doctor.update($scope.patient);
+        $state.go('doctors');
+    }
+  })
+;
