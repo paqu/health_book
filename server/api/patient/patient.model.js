2d5a7f5ca48fb35e837986f6cb4df156c2907e5b
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2016-01-26 17:31:41 +0100

    Add new parentId field to patient model

--- server/api/patient/patient.model.js
+++ server/api/patient/patient.model.js
@@ -4,6 +4,7 @@ var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 var Schema = mongoose.Schema;
 
 var PatientSchema = new Schema({
+    parentId:String,
     childInfo:{
       surname: String,
       firstname:String,
