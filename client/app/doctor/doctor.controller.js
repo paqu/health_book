7d7e17bb9e991d80e89e9a39221a9958aa4567dd
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2016-01-30 19:54:40 +0100

    Add remove method --doctors

--- client/app/doctor/doctor.controller.js
+++ client/app/doctor/doctor.controller.js
@@ -3,5 +3,9 @@
 angular.module('ksiazeczkaZdrowiaApp')
   .controller('DoctorCtrl', function ($scope,Doctor) {
     $scope.doctors  = Doctor.query();
-  })
-;
+
+    $scope.deleteDoctor = function (doctor){
+        Doctor.remove({id: doctor._id});
+        $scope.doctors.splice($scope.doctors.indexOf(doctor),1);
+    }
+  });
