4820ff95ce880676fcefb435cdc80c38d68cf7cb
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2016-01-30 19:53:35 +0100

    Add remove method -- parents

--- client/app/parent/parent.controller.js
+++ client/app/parent/parent.controller.js
@@ -3,4 +3,9 @@
 angular.module('ksiazeczkaZdrowiaApp')
   .controller('ParentCtrl', function ($scope,Parent) {
     $scope.parents = Parent.query();
+
+    $scope.deleteParent = function (_parent){
+        Parent.remove({id: _parent._id});
+        $scope.parents.splice($scope.parents.indexOf(_parent),1);
+    }
   });
