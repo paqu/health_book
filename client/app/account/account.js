75a405853835bc409422a2601f6adf10cd539c23
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2016-02-01 12:49:02 +0100

    Set login page as system's home page

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -4,7 +4,7 @@ angular.module('ksiazeczkaZdrowiaApp')
   .config(function($stateProvider) {
     $stateProvider
       .state('login', {
-        url: '/login',
+        url: '/',
         templateUrl: 'app/account/login/login.html',
         controller: 'LoginController',
         controllerAs: 'vm'
