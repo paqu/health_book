08fb3283ead2646db16876295e9fa915d31270cc
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2016-02-01 23:51:01 +0100

    Add doctor edit controller

--- client/app/doctor/doctor.controller.js
+++ client/app/doctor/doctor.controller.js
@@ -8,4 +8,17 @@ angular.module('ksiazeczkaZdrowiaApp')
         Doctor.remove({id: doctor._id});
         $scope.doctors.splice($scope.doctors.indexOf(doctor),1);
     }
-  });
+  })
+  .controller('DoctorEditCtrl',function ($scope, $state, $stateParams,Doctor) {
+    $scope.doctor = Doctor.get({id: $stateParams.id});
+
+
+    $scope.editDoctor = function (){
+        var firstname = $scope.doctor.firstname;
+        var surname = $scope.doctor.surname;
+        var email = $scope.doctor.email;
+        Doctor.update({id: $stateParams.id},{firstname:firstname,surname:surname,email:email});
+        $state.go('doctorsList');
+    }
+  })
+;
